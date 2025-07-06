d0ffb309d7d0da88254167e523bcd0f4240e821b
Anastasia <nastya.il03@mail.ru>
2025-07-06 22:42:42 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 15959;
+        int oneMile = 20;
+        int mile = price / oneMile;
+
+        System.out.println(mile);
+    }
+}
